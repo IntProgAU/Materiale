8db5ae20ab56d95da34feeebd37ddf3b83f31edf
Thomas Andersen <160220070+au728425@users.noreply.github.com>
2024-10-28 13:44:54 +0100

    Update GUI.java version
    updated gui version to 1.1.1

--- web/e24/opgaver/cg1/GUI.java
+++ web/e24/opgaver/cg1/GUI.java
@@ -20,7 +20,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * Keyboard controls and <space> to turnAround.
  * 
  * @author Nikolaj Ignatieff Schwartzbach
- * @version 1.1.0
+ * @version 1.1.1
  *
  */
 public class GUI {
